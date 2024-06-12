246019d4c12a8a836517f872cc5d9a32dca694f0
masehideki <hide624.ball2492@gmail.com>
2024-06-13 00:09:05 +0900

    趣味を紹介する文を出力する機能を追加

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -8,7 +8,21 @@ public class Main {
 //    文字列結合と数値計算結果の結合をして
 //    自分の名前 ＋ 自分の年齢 ＋ ”歳です。”が出力されるようにする
     String name = "間瀬英紀";
-    int age = 20+7-1;
+    int age = 20 + 7 - 1;
     System.out.println(name + age + "歳です");
+
+//    自己紹介を少し追加してコンソールに出力
+//    "私の趣味は" + hobby[1] + "," + … + "です"が出力されるようにする
+    String[] hobby = {"野球", "ゴルフ", "サカナクション", "旅行" };
+    StringBuilder hobbies = new StringBuilder();
+    int iter = 0;
+    for (String s : hobby) {
+      iter++;
+      hobbies.append(s);
+      if (iter != hobby.length) {
+        hobbies.append(",");
+      }
+    }
+    System.out.println("私の趣味は" + hobbies + "です");
   }
 }
