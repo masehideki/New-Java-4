1cb55ef4ef37fbb631437984f07561f5b716efea
masehideki <hide624.ball2492@gmail.com>
2024-06-12 23:12:19 +0900

    第4回課題　文字列・数値を結合して、名前・年齢の出力を実装

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,14 @@
+package org.example;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+
+  public static void main(String[] args) {
+//    文字列結合と数値計算結果の結合をして
+//    自分の名前 ＋ 自分の年齢 ＋ ”歳です。”が出力されるようにする
+    String name = "間瀬英紀";
+    int age = 20+7-1;
+    System.out.println(name + age + "歳です");
+  }
+}
